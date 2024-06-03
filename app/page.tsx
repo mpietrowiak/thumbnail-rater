23e2f9aea75822d25fbb9f903f84b466239754e9
Mateusz Pietrowiak <matas8@protonmail.com>
2024-06-03 17:18:16 +0200

    convex & clerk"

---
+++ app/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import styles from "./page.module.css";
+import { SignInButton } from "@clerk/nextjs";
+import { SignOutButton } from "@clerk/nextjs";
+import { useSession } from "@clerk/clerk-react";
+import { useConvexAuth, useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+export default function Home() {
+  const { isSignedIn } = useSession();
+  const createThubmnail = useMutation(api.thumbnails.createThumbnail);
+  const thumbnails = useQuery(api.thumbnails.getThumbnailsForUser);
+  const { isLoading, isAuthenticated } = useConvexAuth();
+
+  return (
+    <main className={styles.main}>
+      {isAuthenticated ? <SignOutButton /> : <SignInButton />}
+
+      {isAuthenticated && (
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const form = e.target as HTMLFormElement;
+            const formData = new FormData(form);
+            const title = formData.get("title") as string;
+            await createThubmnail({
+              title,
+            });
+            form.reset();
+          }}
+        >
+          <label>Title</label>
+          <input name="title" className="text-black" />
+          <button>Create</button>
+        </form>
+      )}
+
+      {/* {thumbnails?.length || 0} */}
+
+      {thumbnails?.map((thumbnail) => {
+        return <div key={thumbnail._id}>{thumbnail.title}</div>;
+      })}
+    </main>
+  );
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://massive-phoenix-83.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
